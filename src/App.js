b70b27290d75aec71682d8358a899ba8c2a11c6f
Lokeshganni <111337668+Lokeshganni@users.noreply.github.com>
2024-08-17 19:41:02 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,8 @@ const App = () => (
   <Switch>
     <Route exact path="/" component={Home} />
     <Route exact path="/about" component={About} />
-    <Route component={NotFound} />
+    <Route exact path="/not-found" component={NotFound} />
+    <Redirect to="not-found" />
   </Switch>
 )
 
